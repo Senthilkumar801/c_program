73374735cd91342e9bebfe91af29c7893515068a
Senthilkumar801-test <senthil8012003 @gmail.com>
2024-09-03 14:49:51 +0530

    c_program

---
+++ bitwise.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=5,b=9;
+	printf("a = %d,b=%d\n",a,b);
+	printf("a&b = %d\n",a&b);
+        printf("a<<3 = %d\n",a<<3);
+	printf("a>>1 = %d\n",a>>1);
+	printf("a|b = %d\n",a|b);
+	printf("~a = %d\n",a=~a);
+	return 0;
+}
